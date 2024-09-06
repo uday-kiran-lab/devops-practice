84ed375822b8f0896051c13a076fe3b31c4cda1d
Udaykiran <udaykiran.e98@gmail.com>
2024-09-06 09:16:06 +0530

    one method of variables declared

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#taking the inputs from command prompt
+
+echo "Enter the username::"
+read username
+echo "Enter the username:$username"
+
+echo "Enter the password ::"
+# read password # this will display the input to the user
+# for senstive information you should not display the inputs to uesr 
+read -s password # it will  not dispaly the input to user
+echo "Enter the password:$password" 
