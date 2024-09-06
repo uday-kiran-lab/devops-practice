acf58050ac68fb035e5de307c0f4a7c6e62a1f98
Udaykiran <udaykiran.e98@gmail.com>
2024-09-06 08:56:25 +0530

    wrote conversation scripts

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#conversation between two persons
+
+echo "Ramesh: Hi suresh, How are you?"
+echo "Suresh: Hello Ramesh, i am good.how are you doing?"
+echo "Ramesh: I am doing good suresh.what's going on?"
+echo "Suresh: I started learning shell scripts"
