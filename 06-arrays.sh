f7f75a55f412e9ede47162e03b4cb0387e50d38d
Udaykiran <udaykiran.e98@gmail.com>
2024-09-14 20:41:29 +0530

    updated

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -8,6 +8,6 @@ echo " fisrt friut is:${Friuts[0]}" # accessing the 1st element
 echo " fisrt friut is:${Friuts[1]}" # accessing the 2nd element
 echo " fisrt friut is:${Friuts[2]}" # accessing the 3rd element
 echo " fisrt friut is:${Friuts[3]}" # accessing the 4th element
-echo " fisrt friut is:${Friuts[@]}" # accessing the all elements in array
+echo " All friuts is:${Friuts[@]}" # accessing the all elements in array
 
 # For access the elements in array you should call with their index position
