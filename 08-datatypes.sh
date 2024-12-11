7a9d852977d649803c11f1869f630a7d22c2a280
Udaykiran <udaykiran.e98@gmail.com>
2024-12-11 16:58:55 +0530

    git-practice

---
+++ 08-datatypes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Num1=$1
+Num2=$2
+
+SUM=$($Num1+$Num2)
+
+echo "The total Sum of two numbers is :$SUM"
---
+++ 10-conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Num=$1
+if [ $Num -gt 20 ]
+then
+    echo " The $Num is greaterthan 20"
+else
+    echo " The $Num is lessthan 20"
+fi
